a150c384a0f0677378b71e90ff181860ff31d4a3
Cussa <pablo.cussa@myq-solution.com>
2017-08-29 20:39:46 +0200

    Including the mock webservice for tests

---
+++ src/Web/ProductService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ProductService.asmx.cs" Class="Web.ProductService" %>
---
+++ src/Web/ProductService.asmx.cs
@@ -0,0 +1,63 @@
+﻿using HodStudio.XitSoap.Tests.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Web
+{
+    /// <summary>
+    /// Summary description for Products
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ProductService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public ProductOutput GetProduct()
+        {
+            return CreateProduct();
+        }
+
+        [WebMethod]
+        public bool IsValid()
+        {
+            return true;
+        }
+
+        [WebMethod]
+        public ProductOutput SearchProduct(ProductFilter input)
+        {
+            return CreateProduct();
+        }
+
+        private ProductOutput CreateProduct()
+        {
+            var product = new Product()
+            {
+                Name = "Product 01",
+                Code = 1,
+                Brand = "Brand 01",
+                ExpirationDate = new DateTime(2017, 12, 23),
+                DeliveryDate = new DateTime(2017, 07, 03),
+                SubTitle = "Product 01 Subtitle",
+                Price = 18.30M
+            };
+
+
+            var productOutput = new ProductOutput() { Products = new List<Product>() { product } };
+            return productOutput;
+        }
+    }
+}
